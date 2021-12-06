60d812bccea60198e303f78529180205d207d06d
Teodor Jenkler <32197435+TeodorJenkler@users.noreply.github.com>
2021-12-06 14:57:04 +0100

    Create forex.php

---
+++ forex.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html lang="sv">
+    <head>
+      <meta charset="utf-8"/>
+      <title>Växel</title>
+  </head>
+  <body>
+    <?php
+      require("functions.php");
+   </body>
+</html>
